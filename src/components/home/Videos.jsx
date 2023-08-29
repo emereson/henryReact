b2ee6608bddf7786f8debf4c8d2d22fa24d37c16
emereson <escanorrosa123@gmail.com>
2023-08-28 20:47:34 -0500

    hola

--- src/components/home/Videos.jsx
+++ src/components/home/Videos.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const Videos = ({ video }) => {
+  const videoRef = useRef(null);
+
+  const handleVideoEnded = () => {
+    videoRef.current.play();
+  };
   return (
     <div className="video__container">
-      <video autoPlay muted loop className="video__video">
+      <video
+        autoPlay
+        muted
+        loop
+        onEnded={handleVideoEnded}
+        ref={videoRef}
+        className="video__video"
+      >
         <source src={video.homeVideoUrl} type="video/mp4" />
       </video>
     </div>
